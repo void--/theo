df83de4e00d8063300d970488aa40258091d6e51
Theo Ballew <theoballew@gmail.com>
2018-03-11 20:21:38 -0700

    refactor header component to wait until animation is finished to add/remove links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-export default () => (
-    <header className="header">
-        <Link className="back-link" to="/">Home</Link>
+export default ({path}) => {
+    const showLink = path;
+    const image = <img className="me__image" src="./images/me.png" alt="it's me!"/>;
+
+    const content = (
         <figure className="me">
-            <Link to="/">
-                <img className="me__image" src="./images/me.png" alt="it's me!"/>
-            </Link>
+            {showLink ? <Link to="/">{image}</Link> : image}
             <figcaption className="me__caption">
                 <p className="me__name">Theo Ballew</p>
                 <a className="me__email" target="_blank" rel="noopener noreferrer" href="mailto:theoballew@gmail.com">theoballew@gmail.com</a>
             </figcaption>
         </figure>
-    </header>
-);
+    );
+
+    return (
+        <header className="header">
+                {showLink ? <Link className="back-link" to="/">Home</Link> : <span className="back-link"></span>}
+            {content}
+        </header>
+    );
+};
